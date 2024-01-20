dbc20932774ea7c62f229d7d4b88121c7127e6f3
smg <2289424711@qq.com>
2024-01-20 15:30:26 +0800

    SysRoleController

--- service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
@@ -3,24 +3,58 @@ package com.atguigu.auth.controller;
 import com.atguigu.auth.model.system.SysRole;
 import com.atguigu.auth.service.SysRoleService;
 
+import com.atguigu.auth.vo.system.SysRoleQueryVo;
 import com.atguigu.common.result.Result;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(tags = "角色管理")
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
 
     @Autowired
     private SysRoleService sysRoleService;
-
+    @ApiOperation(value = "获取全部角色列表")
     @GetMapping("findAll")
     public Result<List<SysRole>> findAll() {
         List<SysRole> roleList = sysRoleService.list();
         return Result.ok(roleList);
+    }
+    //条件分页查询
+//page 当前页  limit 每页显示记录数
+//SysRoleQueryVo 条件对象
+    @ApiOperation("条件分页查询")
+    @GetMapping("{page}/{limit}")
+    public Result pageQueryRole(@PathVariable Long page,
+                                @PathVariable Long limit,
+                                SysRoleQueryVo sysRoleQueryVo) {
+        //调用service的方法实现
+        //1 创建Page对象，传递分页相关参数
+        //page 当前页  limit 每页显示记录数
+        Page<SysRole> pageParam = new Page<>(page,limit);
+
+        //2 封装条件，判断条件是否为空，不为空进行封装
+        LambdaQueryWrapper<SysRole> wrapper = new LambdaQueryWrapper<>();
+        String roleName = sysRoleQueryVo.getRoleName();
+        if(!StringUtils.isEmpty(roleName)) {
+            //封装 like模糊查询
+            wrapper.like(SysRole::getRoleName,roleName);
+        }
+
+        //3 调用方法实现
+        IPage<SysRole> pageModel = sysRoleService.page(pageParam, wrapper);
+        return Result.ok(pageModel);
     }
 }
