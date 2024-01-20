e749083321a1ca608c59d617f4693abf86474a85
smg <2289424711@qq.com>
2024-01-20 15:01:57 +0800

    SysRoleController

--- service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/atguigu/auth/controller/SysRoleController.java
@@ -3,6 +3,7 @@ package com.atguigu.auth.controller;
 import com.atguigu.auth.model.system.SysRole;
 import com.atguigu.auth.service.SysRoleService;
 
+import com.atguigu.common.result.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,8 @@ public class SysRoleController {
     private SysRoleService sysRoleService;
 
     @GetMapping("findAll")
-    public List<SysRole> findAll() {
+    public Result<List<SysRole>> findAll() {
         List<SysRole> roleList = sysRoleService.list();
-        return roleList;
+        return Result.ok(roleList);
     }
 }
