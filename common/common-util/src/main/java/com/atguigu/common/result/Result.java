81607dffd35ce03eb88069b17e356c11eaf177c9
smg <2289424711@qq.com>
2024-01-20 14:55:57 +0800

    全局统一返回结果类

--- common/common-util/src/main/java/com/atguigu/common/result/Result.java
+++ common/common-util/src/main/java/com/atguigu/common/result/Result.java
@@ -1,51 +1,76 @@
 package com.atguigu.common.result;
 
+
 import lombok.Data;
 
+/**
+ * 全局统一返回结果类
+ *
+ */
 @Data
 public class Result<T> {
 
-    private Integer code;//状态码
-    private String message;//返回信息
-    private T data;//数据
+    //返回码
+    private Integer code;
 
-    //私有化
-    private Result(){
+    //返回消息
+    private String message;
 
+    //返回数据
+    private T data;
+
+    public Result(){}
+
+    // 返回数据
+    protected static <T> Result<T> build(T data) {
+        Result<T> result = new Result<T>();
+        if (data != null)
+            result.setData(data);
+        return result;
     }
 
-    //封装返回是数据
+    public static <T> Result<T> build(T body, Integer code, String message) {
+        Result<T> result = build(body);
+        result.setCode(code);
+        result.setMessage(message);
+        return result;
+    }
+
     public static <T> Result<T> build(T body, ResultCodeEnum resultCodeEnum) {
-        Result<T> result = new Result<>();
-
-        //封装数据
-        if(body != null){
-            result.setData(body);
-        }
-
-        //状态码
+        Result<T> result = build(body);
         result.setCode(resultCodeEnum.getCode());
-        //返回信息
         result.setMessage(resultCodeEnum.getMessage());
         return result;
     }
 
-    //成功
     public static<T> Result<T> ok(){
-        return Result.build(null, ResultCodeEnum.SUCCESS);
+        return Result.ok(null);
     }
 
+    /**
+     * 操作成功
+     * @param data  baseCategory1List
+     * @param <T>
+     * @return
+     */
     public static<T> Result<T> ok(T data){
-        return Result.build(data, ResultCodeEnum.SUCCESS);
+        Result<T> result = build(data);
+        return build(data, ResultCodeEnum.SUCCESS);
     }
 
-    //失败
     public static<T> Result<T> fail(){
-        return build(null, ResultCodeEnum.FAIL);
+        return Result.fail(null);
     }
 
+    /**
+     * 操作失败
+     * @param data
+     * @param <T>
+     * @return
+     */
     public static<T> Result<T> fail(T data){
-        return Result.build(data, ResultCodeEnum.FAIL);
+        Result<T> result = build(data);
+        return build(data, ResultCodeEnum.FAIL);
     }
 
     public Result<T> message(String msg){
