e01e2db5bd345e17119d07c9564ea8e9f7cb74b4
smg <2289424711@qq.com>
2024-01-20 16:22:45 +0800

    GlobalExceptionHandler

--- common/service-util/src/main/java/com/atguigu/common/config/exception/GlobalExceptionHandler.java
+++ common/service-util/src/main/java/com/atguigu/common/config/exception/GlobalExceptionHandler.java
@@ -1,28 +1,34 @@
 package com.atguigu.common.config.exception;
 
 import com.atguigu.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestControllerAdvice
+
+/**
+ * 全局异常处理类
+ *
+ */
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
-    //全局异常处理, 执行的方法
     @ExceptionHandler(Exception.class)
+    @ResponseBody
     public Result error(Exception e){
         e.printStackTrace();
-        return Result.fail().message("执行了全局异常处理...");
+        return Result.fail();
     }
 
-    //特定异常处理
     @ExceptionHandler(ArithmeticException.class)
+    @ResponseBody
     public Result error(ArithmeticException e){
         e.printStackTrace();
-        return Result.fail().message("执行了特定异常处理...");
+        return Result.fail().message("执行了特定异常处理");
     }
-
-    //自定义异常处理
+    //自定义异常
     @ExceptionHandler(GuiguException.class)
+    @ResponseBody
     public Result error(GuiguException e){
         e.printStackTrace();
         return Result.fail().code(e.getCode()).message(e.getMsg());
