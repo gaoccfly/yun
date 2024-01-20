c8f10cb5819cf3ffa7a5450a559e9e9c2f3663e7
smg <2289424711@qq.com>
2024-01-20 16:23:15 +0800

    GuiguException

--- common/service-util/src/main/java/com/atguigu/common/config/exception/GuiguException.java
+++ common/service-util/src/main/java/com/atguigu/common/config/exception/GuiguException.java
@@ -3,22 +3,24 @@ package com.atguigu.common.config.exception;
 import com.atguigu.common.result.ResultCodeEnum;
 import lombok.Data;
 
+/**
+ * 自定义全局异常类
+ *
+ */
 @Data
 public class GuiguException extends RuntimeException {
 
-    private Integer code; //状态码
-    private String msg; //描述信息
+    private Integer code;
+
+    private String msg;
 
     public GuiguException(Integer code, String msg) {
-        super(msg);
         this.code = code;
         this.msg = msg;
     }
-
-    /**
-     * 接收枚举类型对象
-     * @param resultCodeEnum
-     */
+//         * 接收枚举类型对象
+//     * @param resultCodeEnum
+//
     public GuiguException(ResultCodeEnum resultCodeEnum) {
         super(resultCodeEnum.getMessage());
         this.code = resultCodeEnum.getCode();
@@ -27,9 +29,9 @@ public class GuiguException extends RuntimeException {
 
     @Override
     public String toString() {
-        return "GuliException{" +
+        return "GuiguException{" +
                 "code=" + code +
-                ", message=" + this.getMessage() +
+                ", msg='" + msg + '\'' +
                 '}';
     }
 }
