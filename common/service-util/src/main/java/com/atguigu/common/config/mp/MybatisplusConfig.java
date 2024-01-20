906e49dc6ebec92cc0bae6e8666a854f494bedb7
smg <2289424711@qq.com>
2024-01-20 15:14:52 +0800

    分页插件

--- common/service-util/src/main/java/com/atguigu/common/config/mp/MybatisplusConfig.java
+++ common/service-util/src/main/java/com/atguigu/common/config/mp/MybatisplusConfig.java
@@ -4,11 +4,13 @@ import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MybatisplusConfig {
+@MapperScan("com.atguigu.auth.mapper")
+public class MybatisPlusConfig {
 
     /**
      * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
@@ -24,5 +26,4 @@ public class MybatisplusConfig {
     public ConfigurationCustomizer configurationCustomizer() {
         return configuration -> configuration.setUseDeprecatedExecutor(false);
     }
-
 }
