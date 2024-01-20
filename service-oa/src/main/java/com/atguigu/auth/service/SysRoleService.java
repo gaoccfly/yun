b25a6d85ea02d15525f3e547c81cba75d3d306e1
smg <2289424711@qq.com>
2024-01-20 15:03:52 +0800

    SysRoleService

--- service-oa/src/main/java/com/atguigu/auth/service/SysRoleService.java
+++ service-oa/src/main/java/com/atguigu/auth/service/SysRoleService.java
@@ -1,22 +1,7 @@
 package com.atguigu.auth.service;
 
-import com.atguigu.model.system.SysRole;
-import com.atguigu.vo.system.AssginRoleVo;
-import com.atguigu.vo.system.SysRoleQueryVo;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.atguigu.auth.model.system.SysRole;
 import com.baomidou.mybatisplus.extension.service.IService;
 
-import java.util.Map;
-
-public interface SysRoleService extends IService<SysRole> {
-
-    //条件分页查询
-    IPage<SysRole> pageQueryRole(Page<SysRole> pageParam, SysRoleQueryVo sysRoleQueryVo);
-
-    //查询所有角色和当前用户所属角色
-    Map<String, Object> findRoleDataByUserId(Long userId);
-
-    //为用户分配角色
-    void doAssign(AssginRoleVo assginRoleVo);
+public interface SysRoleService  extends IService<SysRole> {
 }
