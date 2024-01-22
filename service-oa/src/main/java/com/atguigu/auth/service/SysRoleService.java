939e0b0d9baa1a590a0f1a8c304d81660bea2b05
smg <2289424711@qq.com>
2024-01-22 13:27:26 +0800

    SysUserService

--- service-oa/src/main/java/com/atguigu/auth/service/SysRoleService.java
+++ service-oa/src/main/java/com/atguigu/auth/service/SysRoleService.java
@@ -1,7 +1,15 @@
 package com.atguigu.auth.service;
 
 import com.atguigu.auth.model.system.SysRole;
+import com.atguigu.auth.model.system.SysUserRole;
+import com.atguigu.auth.vo.system.AssginRoleVo;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.Map;
+
 public interface SysRoleService  extends IService<SysRole> {
+    Map<String, Object> findRoleDataByUserId(Long userId);
+
+    void doAssign(AssginRoleVo assginRoleVo);
+
 }
