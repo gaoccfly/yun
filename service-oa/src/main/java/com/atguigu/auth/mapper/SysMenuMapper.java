ceb22644ac40fa98b499617b675444e0cfd057b4
smg <2289424711@qq.com>
2024-01-22 18:42:42 +0800

    SysUserRoleMapper

--- service-oa/src/main/java/com/atguigu/auth/mapper/SysMenuMapper.java
+++ service-oa/src/main/java/com/atguigu/auth/mapper/SysMenuMapper.java
@@ -3,6 +3,9 @@ package com.atguigu.auth.mapper;
 
 import com.atguigu.auth.model.system.SysMenu;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,5 +16,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2024-01-21
  */
 public interface SysMenuMapper extends BaseMapper<SysMenu> {
+    List<SysMenu> findListByUserId(@Param("userId") Long userId);
 
+    List<SysMenu> findMenuListByUserId(@Param("userId") Long userId);
 }
