44769efbf49a910aa1e4d9a00dc027bc433a4ef2
smg <2289424711@qq.com>
2024-01-22 13:31:46 +0800

    SysRoleMenuMapper

--- service-oa/src/main/java/com/atguigu/auth/mapper/SysRoleMenuMapper.java
+++ service-oa/src/main/java/com/atguigu/auth/mapper/SysRoleMenuMapper.java
@@ -1,17 +1,11 @@
 package com.atguigu.auth.mapper;
 
-import com.atguigu.model.system.SysRoleMenu;
+import com.atguigu.auth.model.system.SysRoleMenu;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
-/**
- * <p>
- * 角色菜单 Mapper 接口
- * </p>
- *
- * @author roy
- * @since 2023-03-23
- */
+@Repository
 @Mapper
 public interface SysRoleMenuMapper extends BaseMapper<SysRoleMenu> {
 
