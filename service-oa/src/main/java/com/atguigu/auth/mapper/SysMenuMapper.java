270058f26e248baf8806d55120c94dbe3d92281a
smg <2289424711@qq.com>
2024-01-22 13:32:39 +0800

    SysMenuMapper

--- service-oa/src/main/java/com/atguigu/auth/mapper/SysMenuMapper.java
+++ service-oa/src/main/java/com/atguigu/auth/mapper/SysMenuMapper.java
@@ -1,18 +1,17 @@
 package com.atguigu.auth.mapper;
 
-import com.atguigu.model.system.SysMenu;
+
+import com.atguigu.auth.model.system.SysMenu;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
  * 菜单表 Mapper 接口
  * </p>
  *
- * @author roy
- * @since 2023-03-23
+ * @author smg
+ * @since 2024-01-21
  */
-@Mapper
 public interface SysMenuMapper extends BaseMapper<SysMenu> {
 
 }
