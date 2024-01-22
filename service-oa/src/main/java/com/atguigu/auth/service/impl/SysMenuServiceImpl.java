b03314591645591992d38f4116426f24efde7c23
smg <2289424711@qq.com>
2024-01-22 18:43:14 +0800

    SysMenuServiceImpl

--- service-oa/src/main/java/com/atguigu/auth/service/impl/SysMenuServiceImpl.java
+++ service-oa/src/main/java/com/atguigu/auth/service/impl/SysMenuServiceImpl.java
@@ -1,19 +1,26 @@
 package com.atguigu.auth.service.impl;
 
+
 import com.atguigu.auth.mapper.SysMenuMapper;
+import com.atguigu.auth.mapper.SysRoleMenuMapper;
+import com.atguigu.auth.model.system.SysMenu;
+import com.atguigu.auth.model.system.SysRoleMenu;
 import com.atguigu.auth.service.SysMenuService;
 import com.atguigu.auth.service.SysRoleMenuService;
 import com.atguigu.auth.utils.MenuHelper;
+import com.atguigu.auth.vo.system.AssginMenuVo;
+import com.atguigu.auth.vo.system.MetaVo;
+import com.atguigu.auth.vo.system.RouterVo;
 import com.atguigu.common.config.exception.GuiguException;
-import com.atguigu.model.system.SysMenu;
-import com.atguigu.model.system.SysRoleMenu;
-import com.atguigu.vo.system.AssginMenuVo;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,14 +29,16 @@ import java.util.stream.Collectors;
  * 菜单表 服务实现类
  * </p>
  *
- * @author roy
- * @since 2023-03-23
+ * @author smg
+ * @since 2024-01-21
  */
 @Service
 public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> implements SysMenuService {
 
     @Resource
     private SysRoleMenuService sysRoleMenuService;
+    @Resource
+    private  SysMenuMapper sysMenuMapper;
 
     //菜单列表
     @Override
@@ -105,4 +114,104 @@ public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> impl
             sysRoleMenuService.save(sysRoleMenu);
         }
     }
+
+    @Override
+    public List<RouterVo> findUserMenuListByUserId(Long userId) {
+        List<SysMenu> sysMenusList = null;
+        // 1、判断当前用户是否是管理员       userId=1 是管理员
+        // 1.1、 如果是管理员，查询所有菜单列表
+        if (userId.longValue() == 1) {
+            // 查询所有菜单列表
+            LambdaQueryWrapper<SysMenu> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(SysMenu::getStatus, 1);
+            queryWrapper.orderByAsc(SysMenu::getSortValue);
+            sysMenusList = baseMapper.selectList(queryWrapper);
+        } else {
+            // 1.2、如果不是管理员，根据 userId 查询可以操作菜单列表
+            // 多表关联查询:sys_role、sys_role_mexnu、sys_menu
+            sysMenusList = baseMapper.findMenuListByUserId(userId);
+        }
+
+
+        // 2、把查询出来的数据列表， 构建成框架要求的路由结构
+        // 先构建树形结构
+        List<SysMenu> sysMenuTreeList = MenuHelper.buildTree(sysMenusList);
+        // 构建框架要求的路由结构
+        List<RouterVo> routerList = this.buildRouter(sysMenuTreeList);
+
+        return routerList;
+    }
+
+    private List<RouterVo> buildRouter(List<SysMenu>  menus) {
+        // 创建 list 集合，存值最终数据
+        List<RouterVo> routers = new ArrayList<>();
+        // menus 遍历
+        for (SysMenu menu : menus) {
+            RouterVo router = new RouterVo();
+            router.setHidden(false);
+            router.setAlwaysShow(false);
+            router.setPath(getRouterPath(menu));
+            router.setComponent(menu.getComponent());
+            router.setMeta(new MetaVo(menu.getName(), menu.getIcon()));
+            // 下一层数据
+            List<SysMenu> children = menu.getChildren();
+            if (menu.getType().intValue() == 1) {
+                // 加载隐藏路由
+                List<SysMenu> hiddenMenuList = children.stream().filter(item -> !StringUtils.isEmpty(item.getComponent())).collect(Collectors.toList());
+                for (SysMenu hiddenMenu : hiddenMenuList) {
+                    RouterVo hiddenRouter = new RouterVo();
+                    hiddenRouter.setHidden(true);
+                    hiddenRouter.setAlwaysShow(false);
+                    hiddenRouter.setPath(getRouterPath(hiddenMenu));
+                    hiddenRouter.setComponent(hiddenMenu.getComponent());
+                    hiddenRouter.setMeta(new MetaVo(hiddenMenu.getName(), hiddenMenu.getIcon()));
+                    routers.add(hiddenRouter);
+                }
+            }else {
+                if (!CollectionUtils.isEmpty(children)) {
+                    if(children.size() > 0) {
+                        router.setAlwaysShow(true);
+                    }
+                    // 递归
+                    router.setChildren(buildRouter(children));
+                }
+            }
+            routers.add(router);
+        }
+        return routers;
+
+    }
+
+
+    /**
+     * 获取路由地址
+     *
+     * @param menu 菜单信息
+     * @return 路由地址
+     */
+    public String getRouterPath(SysMenu menu) {
+        String routerPath = "/" + menu.getPath();
+        if (menu.getParentId().intValue() != 0) {
+            routerPath = menu.getPath();
+        }
+        return routerPath;
+    }
+
+
+
+    @Override
+    public List<String> findUserPermsByUserId(Long userId) {
+        //超级管理员admin账号id为：1
+        List<SysMenu> sysMenuList = null;
+        if (userId.longValue() == 1) {
+            sysMenuList = this.list(new LambdaQueryWrapper<SysMenu>().eq(SysMenu::getStatus, 1));
+        } else {
+            sysMenuList = sysMenuMapper.findMenuListByUserId(userId);
+        }
+        List<String> permsList = sysMenuList.stream().filter(item -> item.getType() == 2).map(item -> item.getPerms()).collect(Collectors.toList());
+        return permsList;
+    }
 }
+
+
+
