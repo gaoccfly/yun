3177dd13f309a9035c55f4b8986b1e39c629623e
smg <2289424711@qq.com>
2024-01-22 13:32:19 +0800

    IndexController

--- service-oa/src/main/java/com/atguigu/auth/controller/IndexController.java
+++ service-oa/src/main/java/com/atguigu/auth/controller/IndexController.java
@@ -1,7 +1,10 @@
 package com.atguigu.auth.controller;
 
+import com.atguigu.auth.service.SysMenuService;
+import com.atguigu.auth.service.SysUserService;
 import com.atguigu.common.result.Result;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,32 +13,50 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
-@Api(tags = "后台登陆管理")
+/**
+ * <p>
+ * 后台登录登出
+ * </p>
+ */
+@Api(tags = "后台登录管理")
 @RestController
 @RequestMapping("/admin/system/index")
 public class IndexController {
 
-    //login
-    @PostMapping("/login")
-    public Result login(){
+    @Autowired
+    private SysUserService sysUserService;
+
+    @Autowired
+    private SysMenuService sysMenuService;
+    /**
+     * 登录
+     * @return
+     */
+    @PostMapping("login")
+    public Result login() {
         Map<String, Object> map = new HashMap<>();
-        map.put("token", "admin-token");
+        map.put("token","admin");
         return Result.ok(map);
     }
-
-    //info
-    @GetMapping("/info")
-    public Result info(){
+    /**
+     * 获取用户信息
+     * @return
+     */
+    @GetMapping("info")
+    public Result info() {
         Map<String, Object> map = new HashMap<>();
         map.put("roles","[admin]");
         map.put("name","admin");
         map.put("avatar","https://oss.aliyuncs.com/aliyun_id_photo_bucket/default_handsome.jpg");
         return Result.ok(map);
     }
-
-    //退出
-    @PostMapping("/logout")
+    /**
+     * 退出
+     * @return
+     */
+    @PostMapping("logout")
     public Result logout(){
         return Result.ok();
     }
+
 }
