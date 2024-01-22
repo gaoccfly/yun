be49e34abd032bb7f22af50ab6cc999ea4226b61
smg <2289424711@qq.com>
2024-01-22 18:44:38 +0800

    SysUserServiceImpl

--- service-oa/src/main/java/com/atguigu/auth/service/impl/SysUserServiceImpl.java
+++ service-oa/src/main/java/com/atguigu/auth/service/impl/SysUserServiceImpl.java
@@ -1,66 +1,45 @@
 package com.atguigu.auth.service.impl;
 
 import com.atguigu.auth.mapper.SysUserMapper;
+import com.atguigu.auth.model.system.SysUser;
 import com.atguigu.auth.service.SysUserService;
-import com.atguigu.model.system.SysUser;
-import com.atguigu.vo.system.SysUserQueryVo;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
 
 /**
  * <p>
  * 用户表 服务实现类
  * </p>
  *
- * @author roy
- * @since 2023-03-09
+ * @author smg
+ * @since 2024-01-21
  */
+
 @Service
+@Slf4j
 public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> implements SysUserService {
 
-
-
-    //用户条件分页查询
+    // 更新状态
     @Override
-    public IPage<SysUser> index(Page<SysUser> pageParam, SysUserQueryVo sysUserQueryVo) {
-        LambdaQueryWrapper<SysUser> wrapper = new LambdaQueryWrapper<>();
-
-        String username = sysUserQueryVo.getKeyword();
-        String createTimeBegin = sysUserQueryVo.getCreateTimeBegin();
-        String createTimeEnd = sysUserQueryVo.getCreateTimeEnd();
-
-        if(!StringUtils.isEmpty(username)){
-            wrapper.like(SysUser::getUsername, username);
-        }
-
-        if(!StringUtils.isEmpty(createTimeBegin)){
-            wrapper.ge(SysUser::getCreateTime, createTimeBegin);
-        }
-
-        if(!StringUtils.isEmpty(createTimeEnd)){
-            wrapper.le(SysUser::getCreateTime, createTimeEnd);
-        }
-
-
-        return baseMapper.selectPage(pageParam, wrapper);
-    }
-
-    //更新状态
-    @Override
+    @Transactional
     public void updateStatus(Long id, Integer status) {
-        //根据userId查询用户
+        // 根据用户 userid 查询用户对象
         SysUser sysUser = baseMapper.selectById(id);
 
-        //设置修改状态
-        sysUser.setStatus(status);
+        // 设置修改状态
+        if (status == 0 || status == 1) {
+            sysUser.setStatus(status);
+        } else {
+            log.info("数值不合法");
+        }
 
-        //调用方法进行修改
+        // 调用方法进行修改
         baseMapper.updateById(sysUser);
     }
-
-
 }
+
