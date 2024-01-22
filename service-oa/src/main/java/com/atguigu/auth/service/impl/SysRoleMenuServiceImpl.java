5e5ee3bbace74405318317cd1004f4ddd0251bb7
smg <2289424711@qq.com>
2024-01-22 18:43:34 +0800

    SysRoleMenuServiceImpl

--- service-oa/src/main/java/com/atguigu/auth/service/impl/SysRoleMenuServiceImpl.java
+++ service-oa/src/main/java/com/atguigu/auth/service/impl/SysRoleMenuServiceImpl.java
@@ -1,19 +1,11 @@
 package com.atguigu.auth.service.impl;
 
 import com.atguigu.auth.mapper.SysRoleMenuMapper;
+import com.atguigu.auth.model.system.SysRoleMenu;
 import com.atguigu.auth.service.SysRoleMenuService;
-import com.atguigu.model.system.SysRoleMenu;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
-/**
- * <p>
- * 角色菜单 服务实现类
- * </p>
- *
- * @author roy
- * @since 2023-03-23
- */
 @Service
 public class SysRoleMenuServiceImpl extends ServiceImpl<SysRoleMenuMapper, SysRoleMenu> implements SysRoleMenuService {
 
