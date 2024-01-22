1e3c8975bdf7e1bdbe3c1e23c93e7cea3f93a209
smg <2289424711@qq.com>
2024-01-22 13:28:25 +0800

    SysRoleMenuService

--- service-oa/src/main/java/com/atguigu/auth/service/SysRoleMenuService.java
+++ service-oa/src/main/java/com/atguigu/auth/service/SysRoleMenuService.java
@@ -1,16 +1,7 @@
 package com.atguigu.auth.service;
 
-import com.atguigu.model.system.SysRoleMenu;
+import com.atguigu.auth.model.system.SysRoleMenu;
 import com.baomidou.mybatisplus.extension.service.IService;
 
-/**
- * <p>
- * 角色菜单 服务类
- * </p>
- *
- * @author roy
- * @since 2023-03-23
- */
 public interface SysRoleMenuService extends IService<SysRoleMenu> {
-
 }
