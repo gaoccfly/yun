15fad80fa5a335d463ff58735aee9e9d7521681f
smg <2289424711@qq.com>
2024-01-26 10:41:38 +0800

    ServiceAuthApplication

--- service-oa/src/main/java/com/atguigu/auth/ServiceAuthApplication.java
+++ service-oa/src/main/java/com/atguigu/auth/ServiceAuthApplication.java
@@ -2,10 +2,17 @@ package com.atguigu.auth;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//        (exclude = {
+//        //activiti 默认整合security，屏蔽Security认证
+//        SecurityAutoConfiguration.class,
+//        ManagementWebSecurityAutoConfiguration.class
+//})
 @MapperScan("com.atguigu.auth.mapper")
 @ComponentScan("com.atguigu")
 public class ServiceAuthApplication {
